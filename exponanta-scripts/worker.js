6e83907a60b0cc3f45c162f8a4b25d899a8deef8
fozak <i771468@gmail.com>
2025-01-14 21:59:11 -0500

    dc

---
+++ exponanta-scripts/worker.js
@@ -0,0 +1,25 @@
+// worker.js
+self.onmessage = function(event) {
+    const files = event.data;
+    const fileInfoOutput = {};
+
+    files.forEach(file => {
+        const key = file.name; // Get the file name
+        const relativePath = file.webkitRelativePath; // Get the relative path
+
+        // Store only metadata
+        fileInfoOutput[key] = {
+            size: file.size,
+            name: file.name,
+            path: relativePath,
+            last_modified: file.lastModifiedDate ? file.lastModifiedDate.toISOString() : null,
+            created: new Date().toISOString(), // Note: Created date is not available in the browser
+            format: file.type,
+            mimetype: file.type,
+            writable: true,
+            type: "file" // Just marking as a file
+        };
+    });
+
+    postMessage(fileInfoOutput); // Send the results back to the main thread
+};
